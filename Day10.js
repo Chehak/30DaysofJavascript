fe095715af26ba504b2d1df8e7f731c266693a25
Chehak Gupta <gchehak18@gmail.com>
2024-12-10 12:02:05 +0530

    Create Day10.js

---
+++ Day10.js
@@ -0,0 +1,8 @@
+/* 
+Return Length of Arguments Passed
+Write a function argumentsLength that returns the count of arguments passed to it.
+*/
+
+var argumentsLength = function(...args) {
+    return [...args].length
+};
